475c3ba5f48460ce42d72e49a85848cb001d31e7
zeus <zeus.ramirez347@gmail.com>
2021-05-10 22:28:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Zeus Ramirez is the champion"
+document.appendChild(newHeader);
